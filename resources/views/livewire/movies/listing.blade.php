6be75da3399e0268b6bfd93ebd99570ca3119f52
mike.pluquin <mike.pluquin@gmail.com>
2023-11-22 21:35:15 +0100

    Update spacing

--- resources/views/livewire/movies/listing.blade.php
+++ resources/views/livewire/movies/listing.blade.php
@@ -1,6 +1,6 @@
-<div>
+<div class="space-y-4">
     @foreach ($movies as $movie)
-        <div class="bg-white p-8 overflow-hidden shadow-md sm:rounded-lg my-4">
+        <div class="bg-white p-6 shadow-md rounded">
             {{-- Preview each movie --}}
             <livewire:movies.preview
                 :movie="$movie"
